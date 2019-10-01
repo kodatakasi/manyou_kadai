ce0f46715bd1509cef339b3ddfc830e1a413eb50
koda takasi <kodatakasi@gmail.com>
2019-10-01 14:23:28 +0900

    crud機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,17 +8,34 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new
-    if @task.save
-      redirect_to
-    else
-      redirect_to
+    task = Task.new(task_params)
+    if task.save!
+      redirect_to tasks_url, notice: "タスク「#{task.task_name}」を登録しました。"
     end
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.task_name}」を更新しました。"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.task_name}」を削除しました。"
+  end
+  
+  private
+  def task_params
+    params.require(:task).permit(:task_name, :memo)
   end
 end
