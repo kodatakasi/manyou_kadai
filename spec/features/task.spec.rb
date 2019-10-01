75d7b7a6407bdf46a53fc88688bd4d3a42338927
koda takasi <kodatakasi@gmail.com>
2019-10-01 17:02:30 +0900

    wrote rspec

---
+++ spec/features/task.spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.feature "タスク管理機能", type: :feature do
+  scenario "タスク一覧のテスト" do
+    Task.create!(task_name: 'test_task_01', memo: 'testtesttest')
+    Task.create!(task_name: 'test_task_02', memo: 'samplesample')
+
+    visit tasks_path
+
+    expect(page).to have_content 'test_task_01'
+    expect(page).to have_content 'test_task_02'
+  end
+
+  scenario "タスク作成のテスト" do
+    visit new_task_path
+
+    Task.create!(task_name: 'test_task_05', memo: 'testtesttest')
+    Task.create!(task_name: 'test_task_06', memo: 'testkakimasita')
+
+
+    click_on '登録する'
+
+    visit tasks_path
+
+    expect(page).to have_content 'test_task_05'
+    expect(page).to have_content 'test_task_06'
+
+  end
+
+  scenario "タスク詳細のテスト" do
+    Task.create!(task_name: 'test_task_10', memo: 'samplesamplesample')
+
+    visit tasks_path
+
+    click_link 'test_task_10'
+
+    expect(page).to have_content 'samplesamplesample'
+
+  end
+end
