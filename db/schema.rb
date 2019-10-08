f38345a41705cd8d892bffb3b708b93e510d6e2d
koda takasi <kodatakasi@gmail.com>
2019-10-08 11:27:52 +0900

    add column status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_011030) do
+ActiveRecord::Schema.define(version: 2019_10_08_022207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_10_07_011030) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "end_time", default: "2019-12-31"
+    t.integer "status", default: 0, null: false
   end
 
 end
