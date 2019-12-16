0cc33fa87913a55ae511aefcb86c519c865c0d06
kodatakasi <kodatakasi@gmail.com>
2019-12-16 11:15:40 +0900

    add event calender

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,4 +17,8 @@ class Task < ApplicationRecord
   belongs_to :user
   has_many :labellings, dependent: :destroy
   has_many :labels, through: :labellings
+
+  def start_time
+    self.end_time
+  end
 end
